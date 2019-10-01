8697f34fe0b441b8c64d21a169c33fbde15b2986
Mauricio <mauricio@mbp-de-mauricio.home>
2019-10-01 09:19:21 +0100

    added jacoco suport and use GetMapping annotation

--- src/main/java/org/miralles/resume/service/infrastructure/rest/PersonalResumeController.java
+++ src/main/java/org/miralles/resume/service/infrastructure/rest/PersonalResumeController.java
@@ -3,13 +3,12 @@ package org.miralles.resume.service.infrastructure.rest;
 import org.miralles.resume.service.infrastructure.rest.model.ResumeView;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonalResumeController {
-    @RequestMapping(value = "/resume", method = RequestMethod.GET)
+    @GetMapping(value = "/resume")
     public ResponseEntity<ResumeView> getAllEmployees() {
         return new ResponseEntity<>(new ResumeView("This is a test"), HttpStatus.OK);
     }
