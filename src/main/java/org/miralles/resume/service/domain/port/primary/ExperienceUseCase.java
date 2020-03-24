f6cc426df8fc3fb01c7d2e4c4e7c33a5e304a3a4
Mauricio Miralles <mahouu@gmail.com>
2020-03-24 18:21:43 +0000

    RO-50 remove unused code

--- src/main/java/org/miralles/resume/service/domain/port/primary/ExperienceUseCase.java
+++ src/main/java/org/miralles/resume/service/domain/port/primary/ExperienceUseCase.java
@@ -1,11 +1,7 @@
 package org.miralles.resume.service.domain.port.primary;
 
-import org.miralles.resume.service.domain.entity.Experience;
 import org.miralles.resume.service.domain.entity.ExperienceInfo;
 import org.miralles.resume.service.domain.port.secondary.ResumeRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ExperienceUseCase {
     private ResumeRepository resumeRepository;
@@ -16,22 +12,5 @@ public class ExperienceUseCase {
 
     public ExperienceInfo execute(final String language) {
         return resumeRepository.getExperienceInfoBy(language);
-        //return adaptExperienceFrom(allExperienceListFromRepo);
-    }
-
-    private ExperienceInfo adaptExperienceFrom(final List<Experience> allExperienceListFromRepo) {
-        List<Experience> experienceInfoList = new ArrayList<>();
-        for (Experience experienceFromRepo : allExperienceListFromRepo) {
-            Experience experienceAdapted = new Experience(
-                    experienceFromRepo.getTitle(),
-                    experienceFromRepo.getCompany(),
-                    experienceFromRepo.getUrl(),
-                    experienceFromRepo.getRoleDescription(),
-                    experienceFromRepo.getStartDate(),
-                    experienceFromRepo.getEndDate(),
-                    experienceFromRepo.getTasks());
-            experienceInfoList.add(experienceAdapted);
-        }
-        return new ExperienceInfo(experienceInfoList);
     }
 }
