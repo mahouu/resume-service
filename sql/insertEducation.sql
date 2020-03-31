165a495fb80a941a3ff1106c94ebec3b0a1c5d50
Mauricio Miralles <mahouu@gmail.com>
2020-03-31 12:01:03 +0100

    RO-52 added education inserts and experience temp insert

---
+++ sql/insertEducation.sql
@@ -0,0 +1,5 @@
+db.education.insert({ "language":"EN", "startDate":"1997", "endDate":"2000", "title":"Bachelor Technology", "subTitle":"Analist programmer", "description":"Three years of a technical bachelor" })
+
+db.education.insert({ "language":"EN", "startDate":"2015", "title":"Independient course. Software develop.", "subTitle":"Coursera Inc.", "description":"An introduction to interactive programming in python (part 1)" })
+
+db.education.insert({ "language":"EN", "startDate":"2013", "title":"Certification Databases", "subTitle":"Mongo university", "description":"m101j - mongodb certification." })
