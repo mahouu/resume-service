df1e42cc59d56bb709c994d8377a3721d07b269f
mahouu <mahouu@gmail.com>
2020-04-06 14:12:42 +0100

    added some initial data (#17)

--- sql/insertEducation.sql
+++ sql/insertEducation.sql
@@ -1,5 +1,22 @@
 db.education.insert({ "language":"EN", "startDate":"1997", "endDate":"2000", "title":"Bachelor Technology", "subTitle":"Analist programmer", "description":"Three years of a technical bachelor" })
 
+db.education.insert({ "language":"EN", "startDate":"2013", "title":"Certification Databases", "subTitle":"Mongo university", "description":"m101j - mongodb certification." })
+
 db.education.insert({ "language":"EN", "startDate":"2015", "title":"Independient course. Software develop.", "subTitle":"Coursera Inc.", "description":"An introduction to interactive programming in python (part 1)" })
 
-db.education.insert({ "language":"EN", "startDate":"2013", "title":"Certification Databases", "subTitle":"Mongo university", "description":"m101j - mongodb certification." })
+db.education.insert({ "language":"EN", "startDate":"2014", "endDate":"2015", "title":"Hexagonal architecture", "subTitle":"Lastminute.com", "description":"BIT about hexagonal architecture, and several DOJOs about coding, and SOLID." })
+
+db.education.insert({ "language":"EN", "startDate":"2016", "title":"Secondary tests", "subTitle":"Lastminute.com", "description":"BIT about secondary tests and applications into the company." })
+
+db.education.insert({ "language":"EN", "startDate":"2016", "title":"BIT about TDD", "subTitle":"Lastminute.com", "description":"BIT about TDD and applications into the company." })
+
+db.education.insert({ "language":"EN", "startDate":"2016", "title":"BIT code Smells", "subTitle":"Lastminute.com", "description":"BIT about bad smell and how applicaticate thos concepts into the company." })
+
+db.education.insert({ "language":"EN", "startDate":"2018", "title":"BIT DDD", "subTitle":"Solera Inc.", "description":"Internal formation." })
+
+db.education.insert({ "language":"EN", "startDate":"2018", "title":"TDD with Junit 5", "subTitle":"PluralSight.", "description":"" })
+
+db.education.insert({ "language":"EN", "startDate":"2018", "title":"Developing Spark application using scala & cloudera", "subTitle":"PluralSight.", "description":"" })
+
+db.education.insert({ "language":"EN", "startDate":"2018", "title":"Clean architecture: Patterns, Practices and Principles", "subTitle":"PluralSight.", "description":"" })
+
