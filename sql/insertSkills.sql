1bb053c4f85747c3b82bb5a66bf2da95d1d032de
Mauricio Miralles <mahouu@gmail.com>
2020-04-06 14:09:25 +0100

    added some initial data

--- sql/insertSkills.sql
+++ sql/insertSkills.sql
@@ -1,1 +1,5 @@
-db.skill.insert({"language":"EN", "description":"ANY_DESC","level":"ANY_LEVEL","keywords":["key1","key2"]})
+db.skill.insert({"language":"EN", "description":"Web Development","level":"Master","keywords":["html","css","javascript","java","angular","es6","spring","jpa","mongo","rest","microservices","docker","kubernetes"]})
+
+db.skill.insert({"language":"EN", "description":"Java","level":"Master","keywords":["Java","Spring","scala","hexagonal","TDD","Junit","Jmeter","Hibernate","JPA","microservices","REST api","CQRS"]})
+
+db.skill.insert({"language":"EN", "description":"Cloud","level":"Master","keywords":["Google cloud","AWS","Hadoop","Spark","docker","Kubernetes","spring","jpa","mongo","rest","microservices","docker","kubernetes"]})
