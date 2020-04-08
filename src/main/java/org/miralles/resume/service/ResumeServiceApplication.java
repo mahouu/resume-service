73fe7e0560dea5e8bd289f85ee26c25cdf6ce637
Mauricio Miralles <mahouu@gmail.com>
2020-04-08 18:13:46 +0100

    actuator

--- src/main/java/org/miralles/resume/service/ResumeServiceApplication.java
+++ src/main/java/org/miralles/resume/service/ResumeServiceApplication.java
@@ -1,6 +1,5 @@
 package org.miralles.resume.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
