e350fedcb1cbb09ec08a5e7d306f4a8fb5d64f5a
Mauricio Miralles <mahouu@gmail.com>
2020-04-09 16:21:30 +0100

    [RO-39] added prometheus config bean separate

---
+++ src/main/java/org/miralles/resume/service/infrastructure/config/MetricsConfig.java
@@ -0,0 +1,14 @@
+package org.miralles.resume.service.infrastructure.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "ResumeService");
+    }
+}
